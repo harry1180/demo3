0d1edaf0317ca9ef214f52a7f24529048562a1e9
Hari Kishan <30960804+harry1180@users.noreply.github.com>
2024-06-04 13:29:26 -0500

    Update my_table.sql

--- my_table.sql
+++ my_table.sql
@@ -1,4 +1,6 @@
 CREATE TABLE `my_table` (
-  `col1` STRING NOT NULL,
-  `col2` INT NOT NULL
-);
+  `col1` STRING NOT NULL COMMENT 'Description for col1',  -- Add comments for columns (optional)
+  `col2` INT NOT NULL,
+  `col3` ARRAY<STRING> COMMENT 'Array of strings'  -- Example of a complex data type
+)
+PARTITIONED BY (`partition_col` STRING)  -- Add partitioning (optional)
